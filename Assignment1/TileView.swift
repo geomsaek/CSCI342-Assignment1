64018355a25c71512e01e1b662a7535c05951611
geomsaek <msaliba.6@gmail.com>
2016-04-07 14:40:40 +1000

    Part3 files complete: TileView Delegatesand touch behaviour

--- Assignment1/TileView.swift
+++ Assignment1/TileView.swift
@@ -7,9 +7,113 @@
 //
 
 import Foundation
+import UIKit
 
-class TileView {
+class TileView : UIView {
 
-    init(coder aDecoder: NSCoder) {}
+    var img : UIImage?
+    var imgView : UIImageView
+    var tileDelegate : TileViewDelegate?
+    var tileIndex : Int = 0
+    
+    required init?(coder aDecoder: NSCoder) {
+        
+        imgView = UIImageView(frame: CGRect(x: 0, y: 0, width: 69, height: 69))
+        imgView.image = img
+        imgView.translatesAutoresizingMaskIntoConstraints = false
+        
+        super.init(coder: aDecoder)
+        
+        let topConst = NSLayoutConstraint(
+            item: imgView,
+            attribute: NSLayoutAttribute.Top,
+            relatedBy: NSLayoutRelation.Equal,
+            toItem: self,
+            attribute: NSLayoutAttribute.Top,
+            multiplier: 1, constant: 0
+        )
+        
+        let bottomConst = NSLayoutConstraint(
+            item: imgView,
+            attribute: NSLayoutAttribute.Bottom,
+            relatedBy: NSLayoutRelation.Equal,
+            toItem: self,
+            attribute: NSLayoutAttribute.Bottom,
+            multiplier: 1, constant: 0
+        )
+        
+        let rightConst = NSLayoutConstraint(
+            item: imgView,
+            attribute: NSLayoutAttribute.Left,
+            relatedBy: NSLayoutRelation.Equal,
+            toItem: self,
+            attribute: NSLayoutAttribute.Left,
+            multiplier: 1, constant: 0
+        )
+        
+        let leftConst = NSLayoutConstraint(
+            item: imgView,
+            attribute: NSLayoutAttribute.Right,
+            relatedBy: NSLayoutRelation.Equal,
+            toItem: self,
+            attribute: NSLayoutAttribute.Right,
+            multiplier: 1, constant: 0
+        )
+        
+        self.addSubview(imgView)
+        self.addConstraints([bottomConst,topConst,leftConst,rightConst])
+        
+        let tapEvent = UITapGestureRecognizer(target: self, action: #selector(TileView.userAction))
+        self.addGestureRecognizer(tapEvent)
+    
+    }
+    
+    func userAction() {
+        let temp: UIImage? = self.img
+        
+        if ((temp) != nil) {
+            
+            if(tileIndex != self.tag){
+                self.imgView.alpha=0
+                self.imgView.alpha=1
+                
+                self.displayTile()
+                tileDelegate?.didSelectTile(self)
+            }
+        }
+    }
+    
+    // reveal function
+    func displayTile(){
+        
+        let temp: UIImage? = img
+        if ((temp) != nil) {
+            imgView.alpha=0;
+            imgView.image = img;
+            alterOpacity()
+        }
+    }
+    
+    func coverImage() {
+        imgView.alpha=0;
+        imgView.image = UIImage(named: "question.png")!
+        alterOpacity()
+    }
+    
+    func hideTile() {
+        imgView.alpha=0;
+        imgView.image = nil
+        img = nil
+        alterOpacity()
+    }
+    
+    // additional function to handle opacity
+    func alterOpacity(){
+        UIView.animateWithDuration(1,animations: { self.imgView.alpha = 1.0 })
+    }
 
+}
+
+protocol TileViewDelegate {
+    func didSelectTile(tileView: TileView)
 }
--- Assignment1/ViewController.swift
+++ Assignment1/ViewController.swift
@@ -8,11 +8,67 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GameModelDelegate, TileViewDelegate {
+
+    var gameTiles: [TileView]
+    let tileCount = 12
+    var puzzle : GameModel
+    let tiles: [UIImage] = [UIImage(named: "baldhill.png")!, UIImage(named: "cathedral.png")!, UIImage(named: "lake.png")!]
+    
+    required init?(coder aDecoder: NSCoder){
+        
+        puzzle = GameModel(number : tileCount, images : tiles)
+        gameTiles = [TileView]()
+        
+        //print(puzzle)
+        // initialise the superclass
+        super.init(coder: aDecoder)
+        
+        // delegate the game model delegate to the current object
+        puzzle.delegate = self
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        makeTiles()
+    }
+    
+    func makeTiles(){
+        
+        for index in 0..<tileCount {
+            
+            if let tile = self.view.viewWithTag(index+1) as? TileView {
+                gameTiles.append(tile)
+                gameTiles[index].img = puzzle.tiles[index].imageObject
+                gameTiles[index].tileIndex = index
+                gameTiles[index].tileDelegate = self
+                gameTiles[index].coverImage()
+            }
+            
+        }
+    }
+    
+    // if the user selected the tile - from TileView
+    func didSelectTile(tileView: TileView) {
+        puzzle.pushTileIndex(tileView.tileIndex)
+    }
+    
+    
+    func gameDidComplete(gameModel: GameModel){
+    
+    }
+    
+    func didMatchTile(gameModel: GameModel, tileIndex: Int, previousTileIndex: Int) {
+    
+    }
+    
+    func didFailToMatchTile(gameModel: GameModel, tileIndex: Int, previousTileIndex: Int) {
+        
+    }
+    
+    func scoreDidUpdate(gameModel: GameModel, newScore: Int) {
+        
     }
 
     override func didReceiveMemoryWarning() {
