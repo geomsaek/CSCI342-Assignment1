e57675921231942e1a67811984cfe7edd9a7355b
geomsaek <msaliba.6@gmail.com>
2016-04-07 14:23:44 +1000

    Part2 updates: Layout and TileView Class

---
+++ Assignment1/TileView.swift
@@ -0,0 +1,15 @@
+//
+//  TileView.swift
+//  Assignment1
+//
+//  Created by Matthew Saliba on 1/04/2016.
+//  Copyright © 2016 Matthew Saliba. All rights reserved.
+//
+
+import Foundation
+
+class TileView {
+
+    init(coder aDecoder: NSCoder) {}
+
+}
